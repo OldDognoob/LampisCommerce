213c2948e4d7df607e7604bf88a83041e628e166
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-04-15 18:55:03 +0300

    style-correction-file

--- my-lampis/src/components/Products/Product/styles.js
+++ my-lampis/src/components/Products/Product/styles.js
@@ -1,5 +1,8 @@
 import {makeStyles} from '@material-ui/core/styles';
 
+// we exporting something ,  this function takes a parameter, which is a callback function
+//  with an instant return, we rapped in parenthesis and we return an object.
+
 export default makeStyles(()=>({
     root:{
         maxWidth: '100%'
