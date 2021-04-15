cc696cebb6ff626ebf462a8af650fe912bbd5b42
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-04-15 18:52:46 +0300

    create-styles-file

--- my-lampis/src/components/Products/Product/Product.jsx
+++ my-lampis/src/components/Products/Product/Product.jsx
@@ -8,8 +8,10 @@ import {
   IconButton,
 } from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
+import useStyles from './styles';
 
 const Product = ({product}) => {
+    const classes = useStyles();
   return (
     <Card className={classes.root}>
       <CardMedia className={classes.media} image="" title={product.name} />
