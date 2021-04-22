0207e843ce1d204aa8eeeefe38b37a8157742607
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-04-22 14:48:41 +0300

    changes

--- my-lampis/src/App.js
+++ my-lampis/src/App.js
@@ -1,36 +1,31 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { commerce } from "./lib/commerce";
+// import { commerce } from "./lib/commerce";
 
 import { Navbar, Products } from "./components";
 
 const App = () => {
   const [products, setProducts] = useState([]);
 
-  const fetchProducts = async () => {
-    const { data } = await commerce.products.list();
-    setProducts(data);
-    // commerce.products.list().then((products)=>{
-    //     this.setState({products: products.data});
-    // }).catch((error) =>{
-    //     console.log('There was an error fetching the products', error);
-    // });
-  };
+//   const fetchProducts = async () => {
+//     const { data } = await commerce.products.list();
+//     setProducts(data);
+   
 
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+//   useEffect(() => {
+//     fetchProducts();
+//   }, []);
 
-
-
-  console.log(products);
+//   console.log(products);
 
   return (
     <Router>
       <div>
         <Navbar />
         <Switch>
-          <Products products={products} />
+            <Route exact path="/">
+            <Products products={products} />
+            </Route>
         </Switch>
       </div>
     </Router>
